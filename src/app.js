65b8584896f15034d74d67993a7c5159877f7067
ydj-13 <de2013@naver.com>
2026-06-02 09:27:17 +0900

    fix:app.js에 함수이름 수정 안되었던 부분 다시 수정

--- src/app.js
+++ src/app.js
@@ -11,13 +11,12 @@ app.get("/", (req, res) =>
   res.json({ status: "ok", message: "연결 테스트 확인용임" }),
 );
 
-app.get("/api/companies/:id", CorpInvestController.GetCompany);
-app.get("/api/companies/:id/investment", CorpInvestController.GetInvestment);
+app.get("/api/companies/:id", CorpInvestController.getCompany);
+app.get("/api/companies/:id/investment", CorpInvestController.getInvestment);
 
-app.post("/api/companies/:id/investment", CorpInvestController.PostInvestment);
-app.post("/api/investments/:id/password", CorpInvestController.PasswordCheck);
+app.post("/api/investments/:id/password", CorpInvestController.passwordCheck);
 
-app.patch("/api/investments/:id", CorpInvestController.PatchInvestment);
-app.delete("/api/investments/:id", CorpInvestController.DeleteInvestment);
+app.patch("/api/investments/:id", CorpInvestController.patchInvestment);
+app.delete("/api/investments/:id", CorpInvestController.deleteInvestment);
 
 export default app;
