bd076f5f164274cdc535b4e91f1da3c580b18b63
jaelee <seungjael508@gmail.com>
2026-06-02 16:01:04 +0900

    refactor: connect controllers directly in app

--- src/routes/comparisonStats.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express'
-import comparisonStatsController from '../controllers/comparisonStats.controller.js'
-
-const router = Router()
-
-router.get('/', comparisonStatsController.getList)
-
-export default router
