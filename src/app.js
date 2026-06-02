d91020b5a1b74867772f08e28ab42cbceef30813
MinSung Jo <bloomin330@gmail.com>
2026-06-02 11:04:13 +0900

    feat: 투자현황 app.js 라우트 연결

--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import express from "express";
 import cors from "cors";
+import investCompaniesController from "./controllers/investcompanies.controller.js";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.get("/api/investcompanies", investCompaniesController.getInvestCompanies);
 
 app.get("/", (req, res) =>
   res.json({ status: "ok", message: "연결 테스트 확인용임" }),
